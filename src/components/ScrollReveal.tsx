7d3dc7fc92b1b92f4567d09414698301fe9361dd
Jorjo6712 <kiro.nicolov@gmail.com>
2024-09-09 18:33:05 +0200

    trying to fix vercel error

--- src/components/ScrollReveal.tsx
+++ src/components/ScrollReveal.tsx
@@ -1,5 +1,5 @@
 import { motion, useInView, useAnimation } from 'framer-motion'
-import React, { useEffect, useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 interface ScrollRevealProps {
   children?: JSX.Element|JSX.Element[],
