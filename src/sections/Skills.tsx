21f17fbec3ea3915f7d754fa6c4dff5274c34f91
Jorjo6712 <kiro.nicolov@gmail.com>
2024-09-12 14:09:28 +0200

    vercel

--- src/sections/Skills.tsx
+++ src/sections/Skills.tsx
@@ -3,7 +3,12 @@ import { ScrollReveal } from "../components/ScrollReveal"
 export const Skills : React.FC = () => {
   return (
   <>
-    <ScrollReveal x={55} delay={0.565}>   
+    <ScrollReveal 
+      x={55} 
+      delay={0.565}
+      once={true}
+      transitionDuration={0.75}
+    >   
       <h1 className="text-3xl text-[#2D3142]"> Skills works </h1>
     </ScrollReveal>
   </>
